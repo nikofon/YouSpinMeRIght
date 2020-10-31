a2d0316f1c6b0b45ea8ba7f1ba9d9902aa2f1529
nikofon <nikofon007@gmail.com>
2020-10-31 17:21:59 +0300

    new Level:

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public static LevelLoader instance;
+    private int currentLevel;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance != this)
+        {
+            Destroy(gameObject);
+        }   
+    }
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadSameLevel()
+    {
+        LoadLevel(currentLevel);
+    }
+    public void LoadNextLevel()
+    {
+        currentLevel++;
+        LoadLevel(currentLevel);
+    }
+
+
+    public void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+}
---
+++ Assets/Scripts/PlayerWinCondition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWinCondition : MonoBehaviour
+{
+    List<GameObject> targets;
+    private void Start()
+    {
+        targets = new List<GameObject>();
+        targets.AddRange(GameObject.FindGameObjectsWithTag("Target"));
+    }
+    private void Update()
+    {
+        foreach(GameObject t in targets)
+        {
+            if(Vector3.Distance(transform.position, t.transform.position) < 0.1)
+            {
+                targets.Remove(t);
+            }
+        }
+        if(targets.Count == 0)
+        {
+            LevelLoader.instance.LoadNextLevel();
+        }
+    }
+}
